f001b4f8f9b3d32ffa499c4de919c91c1dd560c6
saniazt <saniazt@gmail.com>
2022-10-29 23:31:54 +0300

    1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,52 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 public class FileReader {
+    public static void main(String[] args) {
+        Profile profile = getDataFromFile(new File("src/main/resources/Profile.txt"));
+        System.err.print(profile);
+    }
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public static Profile getDataFromFile(File file) {
+        Profile profile = new Profile();
+        FileInputStream fileInputStream = null;
+        StringBuilder text = new StringBuilder();
+        try {
+            fileInputStream = new FileInputStream(file);
+            int ch = 0;
+            while ((ch = fileInputStream.read()) != -1) {
+                text.append((char) ch);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fileInputStream != null) {
+                    fileInputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        String[] splitText = text.toString().split(System.lineSeparator());
+
+        String[][] arrayText = new String[splitText.length][];
+        for (int i = 0; i < splitText.length; i++) {
+            arrayText[i] = splitText[i].split(" ");
+        }
+
+        profile.setName(arrayText[0][1]);
+        int age = Integer.parseInt(arrayText[1][1]);
+        long phone = Long.parseLong(arrayText[3][1]);
+        profile.setAge(age);
+        profile.setEmail(arrayText[2][1]);
+        profile.setPhone(phone);
+        return new Profile(profile.getName(), profile.getAge(), profile.getEmail(), profile.getPhone());
     }
 }
